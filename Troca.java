9749e9d25c4e4469e68aadeab807ff84a4728ed4
Nicoly dos Santos Dias <santosnico048@gmail.com>
2024-10-03 14:45:38 -0300

    Update Troca.java

--- Troca.java
+++ Troca.java
@@ -12,8 +12,8 @@
        System.out.println("Digite um número para ser definido como valor da variável B: ");
        num2= sc.nextInt();
       
-    System.out.println("Agora a variável A passa a possuir o valor: " + num2);
+    System.out.println("Agora a variável A passa a possuir o valor: " +num2);
       
- System.out.println("Agora a variável B passa a possuir o valor: " + num1);
+ System.out.println("Agora a variável B passa a possuir o valor: " +num1);
      }
     }
